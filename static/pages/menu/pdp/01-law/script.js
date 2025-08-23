600951716ba790dc0f9db3b66a57b50700935946
Rafael Proença <rafarsp89@gmail.com>
2025-08-23 11:43:49 -0300

    coleções e funções

--- static/pages/menu/pdp/01-law/script.js
+++ static/pages/menu/pdp/01-law/script.js
@@ -7,13 +7,3 @@ function apagaLampada() {
   document.getElementById("imagem").src =
     "https://www.w3schools.com/js/pic_bulboff.gif";
 }
-
-
-let x = 6;
-let soma = 0;
-while (x <= 12) {
-  soma += x;
-  x += 2;
-}
-
-console.log(soma);
