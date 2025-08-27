da2f4042ac092d140e64a2a8ebf892a9681b6bad
Rafael Proença <rafarsp89@gmail.com>
2025-08-27 09:06:44 -0300

    projeto mão na massa

--- static/pages/menu/pdp/01-law/script.js
+++ static/pages/menu/pdp/01-law/script.js
@@ -7,3 +7,9 @@ function apagaLampada() {
   document.getElementById("imagem").src =
     "https://www.w3schools.com/js/pic_bulboff.gif";
 }
+
+let x = [1,2,3,4,5];
+x.splice(0,3);
+x.splice(0,1);  
+x.pop();
+console.log(x);
